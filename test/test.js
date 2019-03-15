2f411c991cd051df7a9a431965db4cc9cd0d9fc5
Ganja Vasiliy <vasllly219@gmail.com>
2019-03-15 17:19:16 +0300

    Fix test discription

--- test/test.js
+++ test/test.js
@@ -1,34 +1,34 @@
 import indication from '../src/js/script';
 
-test('should calculate total for purchases', () => {
+test('show indication', () => {
   const input = { name: 'Маг', health: 90 };
   const expected = 'healthy';
   const received = indication(input);
   expect(received).toBe(expected);
 });
 
-test('should calculate total for purchases', () => {
+test('show indication', () => {
   const input = { name: 'Маг', health: 50 };
   const expected = 'wounded';
   const received = indication(input);
   expect(received).toBe(expected);
 });
 
-test('should calculate total for purchases', () => {
+test('show indication', () => {
   const input = { name: 'Маг', health: 30 };
   const expected = 'wounded';
   const received = indication(input);
   expect(received).toBe(expected);
 });
 
-test('should calculate total for purchases', () => {
+test('show indication', () => {
   const input = { name: 'Маг', health: 15 };
   const expected = 'wounded';
   const received = indication(input);
   expect(received).toBe(expected);
 });
 
-test('should calculate total for purchases', () => {
+test('show indication', () => {
   const input = { name: 'Маг', health: 10 };
   const expected = 'critical';
   const received = indication(input);
